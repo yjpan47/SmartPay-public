039f1d796904fc10b12dceba8267cb02efa0a0f5
johnp <johnp@eiqmobility.com>
2020-06-30 15:22:25 +0800

    resolve reloading bug

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log("installed");
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    console.log(changeInfo);
+    if (changeInfo.url !== undefined && changeInfo.url.includes("https://shopee.sg/checkout/?state=")) {
+        console.log('HERE');
+        console.log(tab);
+        chrome.tabs.executeScript(tab.id, {file: "content.js"}, () => {})
+    }
+});
